6c4e8ea6c75af10e1b90a394bf360633abc915e5
Developer <dev@event-ticketing.local>
2026-05-13 01:39:04 +0700

    feat: implement role-based routing and dashboard redirection for the application

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,16 @@ use App\Http\Controllers\Dashboard\ScannerDashboardController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EventController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketCategoryController;
 
 /*
 |--------------------------------------------------------------------------
 | Public Routes
 |--------------------------------------------------------------------------
 */
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/events/{event}', [HomeController::class, 'show'])->name('events.show');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 /*
 |--------------------------------------------------------------------------
